b6507b4d38fc87ffa6c5ba2961d78644eb6fd007
Angelina-riveros <angelinariv24@gmail.com>
2025-12-03 22:44:17 -0500

    Add Render deployment config

--- app.py
+++ app.py
@@ -139,4 +139,6 @@ def vehicles():
     return render_template('vehicles.html', vehicles=fake_vehicles)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
